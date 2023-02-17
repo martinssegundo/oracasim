93d58722b66759000bedd4bcd5af2bafbc0151ce
Luiz Segundo <simoes.segundo@gmail.com>
2023-02-16 22:01:19 -0300

    Gradle configs and SpringBOot starter and test

---
+++ src/main/java/br/com/tasdua/orcasim/OrcasimApplication.java
@@ -0,0 +1,13 @@
+package br.com.tasdua.orcasim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcasimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcasimApplication.class, args);
+	}
+
+}
